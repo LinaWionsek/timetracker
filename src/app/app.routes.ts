61e9ede8a4e30b560d04315b877d012413626443
Lina Wion <lina@test.com>
2025-02-26 14:56:43 +0100

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { TimeLogsComponent } from './time-logs/time-logs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'overview', component: MainComponent },
+    { path: 'logs', component: TimeLogsComponent },
+    { path: '', redirectTo: '/overview', pathMatch: 'full' }
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
