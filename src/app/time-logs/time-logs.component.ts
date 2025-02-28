aae6a8184620774c537aa0dff24288c82511471f
Lina Wion <lina@test.com>
2025-02-28 11:04:46 +0100

    refactor time logs component to use Angular Material table with sorting functionality

--- src/app/time-logs/time-logs.component.ts
+++ src/app/time-logs/time-logs.component.ts
@@ -1,28 +1,57 @@
-import { Component } from '@angular/core';
+
 import {MatCardModule} from '@angular/material/card';
 
+
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {AfterViewInit, Component, ViewChild, inject} from '@angular/core';
+import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+
+interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-time-logs',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatTableModule, MatSortModule],
   templateUrl: './time-logs.component.html',
   styleUrl: './time-logs.component.scss'
 })
-export class TimeLogsComponent {
-  displayedColumns: string[] = ['date', 'timestamp', 'break', 'duration', 'edit'];
- 
-  transactions: any[] = [
-    {item: 'Beach ball', cost: 4},
-    {item: 'Towel', cost: 5},
-    {item: 'Frisbee', cost: 2},
-    {item: 'Sunscreen', cost: 4},
-    {item: 'Cooler', cost: 25},
-    {item: 'Swim suit', cost: 15},
-  ];
-  
-  /** Gets the total cost of all transactions. */
-  getTotalCost() {
-    // return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
+export class TimeLogsComponent implements AfterViewInit {
+  private _liveAnnouncer = inject(LiveAnnouncer);
+
+  displayedColumns: string[] = ['date', 'timestamp', 'break'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
