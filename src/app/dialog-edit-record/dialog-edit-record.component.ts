2facfa3efb67c1eeee073a333ca8120a3424bd58
Lina Wion <lina@test.com>
2025-03-07 18:06:16 +0100

    deleted unused code

--- src/app/dialog-edit-record/dialog-edit-record.component.ts
+++ src/app/dialog-edit-record/dialog-edit-record.component.ts
@@ -2,7 +2,6 @@ import { Component, inject } from '@angular/core';
 import { Timerecords } from '../models/timerecords.class';
 import { MatDialogRef } from '@angular/material/dialog';
 import {MatDialogModule} from '@angular/material/dialog';
-import { DataStoreServiceService } from '../services/data-store-service.service';
 import { TimeRecordFormComponent } from '../time-record-form/time-record-form.component';
 @Component({
   selector: 'app-dialog-edit-record',
@@ -16,24 +15,8 @@ export class DialogEditRecordComponent {
 
   constructor(
     public dialogRef: MatDialogRef<DialogEditRecordComponent>,
-    private dataStoreService: DataStoreServiceService
+    // private dataStoreService: DataStoreServiceService
   ) {}
-
-  onRecordSaved(record: Timerecords) {
-    // Hier "Update" durchführen (falls du es schon implementiert hast)
-    if (record.id) {
-      // updateDoc in Firestore ...
-      // dataStoreService.updateData(record);
-      console.log('Record updated:', record);
-    } else {
-      // Falls kein id → neu anlegen
-      this.dataStoreService.safeData(record);
-      console.log('Record created:', record);
-    }
-
-    // Dialog schließen
-    this.dialogRef.close();
-  }
 
 
   onFormCancel() {
